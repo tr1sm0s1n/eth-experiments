5a820f73fe3f26906f56bb7802adb071947c04bf
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-11-04 14:39:46 +0530

    🚑️ | fix web3.py blob tx example

--- python/blobTx.py
+++ python/blobTx.py
@@ -1,37 +1,33 @@
-import os
-from eth_abi import abi
-from eth_account import Account
-from eth_utils import to_hex
-from web3 import Web3, HTTPProvider
+from web3 import Web3
+from os import getenv
+from dotenv import load_dotenv
+from eth_abi import encode
 
-
-rpc_url = os.environ.get("RPC_URL")
-w3 = Web3(HTTPProvider(rpc_url))
-
-text = "<( o.O )>"
-encoded_text = abi.encode(["string"], [text])
-
-BLOB_DATA = (b"\x00" * 32 * (4096 - len(encoded_text) // 32)) + encoded_text
-
-pkey = os.environ.get("PRIVATE_KEY")
-acct = w3.eth.account.from_key(pkey)
+load_dotenv()
+w3 = Web3(Web3.HTTPProvider(getenv("RPC_URL")))
+eoa = w3.eth.account.from_key(getenv("PRIVATE_KEY"))  # replace with your own set up
 
 tx = {
-    "type": 3,
-    "chainId": w3.eth.chain_id,
-    "from": acct.address,
-    "to": "0x0000000000000000000000000000000000000000",
+    "from": eoa.address,
     "value": 0,
-    "maxFeePerGas": 10**12,
-    "maxPriorityFeePerGas": 10**12,
-    "maxFeePerBlobGas": to_hex(10**12),
-    "nonce": w3.eth.get_transaction_count(acct.address),
+    "chainId": w3.eth.chain_id,
+    "gas": 250000,
+    "to": "0x0000000000000000000000000000000000000000",
+    "maxFeePerGas": w3.eth.gas_price * 2,
+    "maxPriorityFeePerGas": w3.eth.max_priority_fee * 2,
+    "maxFeePerBlobGas": hex(10**12),
+    "nonce": w3.eth.get_transaction_count(eoa.address),
 }
 
-gas_estimate = w3.eth.estimate_gas(tx)
-tx["gas"] = gas_estimate
+text = "<( o.O )>"
+encoded_text = encode(["string"], [text])
 
-signed = acct.sign_transaction(tx, blobs=[BLOB_DATA])
-tx_hash = w3.eth.send_raw_transaction(signed.rawTransaction)
+padding_bytes = b"\x00" * 32 * (4096 - len(encoded_text) // 32)
+
+BLOB_DATA = padding_bytes + encoded_text
+
+signed_tx = eoa.sign_transaction(tx, blobs=[BLOB_DATA])
+tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+print(f"Tx hash: 0x{tx_hash.hex()}")
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 print(f"Tx receipt: {tx_receipt}")
