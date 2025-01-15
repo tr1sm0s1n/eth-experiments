32cecc8c033bf1742915a9ee61c7074752870e12
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-01-15 16:23:50 +0530

    ⚗️ | use the entire row as data

--- _lab/event-query/common/Datastore.sol
+++ _lab/event-query/common/Datastore.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract DataStore {
-    event Stored(string indexed exam_no, string data);
+    event Stored(string indexed exam_no, string[] data);
 
     struct EventInfo {
         uint start;
@@ -14,7 +14,7 @@ contract DataStore {
 
     function storeData(string[][] memory newStrings) public {
         for (uint i = 0; i < newStrings.length; i++) {
-            emit Stored(newStrings[i][3], newStrings[i][1]);
+            emit Stored(newStrings[i][3], newStrings[i]);
             EventCount[newStrings[i][3]].end = block.number;
 
             if (!EventCount[newStrings[i][3]].first) {
