f1cd1b2293ed411a77d652df0b2c332b9bd3a9ea
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-01-25 21:13:58 +0530

    ⚗️ | update log_processor with loop, BLOCK_RANGE, fetch_logs()

--- _lab/event-query/src/bin/log_processor.rs
+++ _lab/event-query/src/bin/log_processor.rs
@@ -1,11 +1,12 @@
 use alloy::{
-    primitives::{keccak256, Address},
-    providers::{Provider, ProviderBuilder},
+    primitives::{keccak256, Address, FixedBytes},
+    providers::{Provider, ProviderBuilder, RootProvider},
     rpc::types::Filter,
     sol,
     sol_types::SolEvent,
+    transports::http::{Client, Http},
 };
-use event_query::constants::{CONTRACT_ADDRESS, EXAM_TITLE, RPC_URL};
+use event_query::constants::{BLOCK_RANGE, CONTRACT_ADDRESS, EXAM_TITLE, RPC_URL};
 use eyre::Result;
 use Datastore::Stored;
 
@@ -28,23 +29,47 @@ async fn main() -> Result<()> {
     // Fetch the block range.
     let range = instance.EventCount(EXAM_TITLE.to_string()).call().await?;
 
+    let filter_topic = keccak256(EXAM_TITLE);
+
+    let mut start = range.start.to::<u64>();
+    let mut end = range.start.to::<u64>() + BLOCK_RANGE;
+
+    loop {
+        if end >= range.end.to::<u64>() {
+            end = range.end.to::<u64>();
+            fetch_logs(&provider, filter_topic, &mut events, start, end).await?;
+            break;
+        }
+        fetch_logs(&provider, filter_topic, &mut events, start, end).await?;
+        start = end + 1;
+        end = end + 1 + BLOCK_RANGE;
+    }
+
+    println!("Processed \x1b[34m{:?}\x1b[0m event logs!!", events.len());
+
+    Ok(())
+}
+
+async fn fetch_logs(
+    provider: &RootProvider<Http<Client>>,
+    filter_topic: FixedBytes<32>,
+    events: &mut Vec<Stored>,
+    start: u64,
+    end: u64,
+) -> Result<()> {
     let filter = Filter::new()
         .address(CONTRACT_ADDRESS.parse::<Address>()?)
         .event(Datastore::Stored::SIGNATURE)
-        .from_block(range.start.to::<u64>())
-        .to_block(range.end.to::<u64>());
+        .from_block(start)
+        .to_block(end);
 
     let logs = provider.get_logs(&filter).await?;
-    let filter_topic = keccak256(EXAM_TITLE);
-
     for l in logs {
         if l.topics()[1] == filter_topic {
             let parsed = Datastore::Stored::decode_log_data(l.data(), true)?;
             events.push(parsed);
         }
     }
-
-    println!("Processed \x1b[34m{:?}\x1b[0m event logs!!", events.len());
 
     Ok(())
 }
