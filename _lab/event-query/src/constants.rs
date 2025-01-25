c465e5fade0ab3c76a1cb45876f727e51d7d683c
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-01-25 20:55:08 +0530

    ⚗️ | add BLOCK_RANGE to constants

--- _lab/event-query/src/constants.rs
+++ _lab/event-query/src/constants.rs
@@ -6,3 +6,5 @@ pub const WS_URL: &str = "ws://127.0.0.1:8546";
 pub const CONTRACT_ADDRESS: &str = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 // Update if required.
 pub const EXAM_TITLE: &str = "TENK";
+// Block range for event log processing.
+pub const BLOCK_RANGE: u64 = 200;
