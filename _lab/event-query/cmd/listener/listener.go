dbd369228e95a12a378897752bd022dbe75db038
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-01-30 17:13:50 +0530

    ⚗️ | add additional listener using WatchStored from binding

--- _lab/event-query/cmd/listener/listener.go
+++ _lab/event-query/cmd/listener/listener.go
@@ -4,6 +4,7 @@ import (
 	cmn "_lab/event-query/common"
 	"context"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/ethereum/go-ethereum"
@@ -13,7 +14,7 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func main() {
+func listenWithClient() {
 	client, err := ethclient.Dial(cmn.WebSocketURL)
 	if err != nil {
 		panic(err)
@@ -44,7 +45,9 @@ func main() {
 		case vLog := <-logs:
 			var event cmn.DatastoreStored
 
-			certABI.UnpackIntoInterface(&event, "Stored", vLog.Data)
+			if err := certABI.UnpackIntoInterface(&event, "Stored", vLog.Data); err != nil {
+				panic(err)
+			}
 			log.Println("Event occured!!")
 			log.Println("--------------------")
 			log.Printf("No: \033[34m%s\033[0m\n", event.ExamNo)
@@ -52,5 +55,61 @@ func main() {
 			log.Printf("Raw log: \033[32m%v\033[0m\n", event.Raw)
 			log.Println("--------------------")
 		}
+	}
+}
+
+func listenWithContract() {
+	client, err := ethclient.Dial(cmn.WebSocketURL)
+	if err != nil {
+		panic(err)
+	}
+
+	instance, err := cmn.NewDatastore(cmn.ContractAddress, client)
+	if err != nil {
+		panic(err)
+	}
+
+	eventChan := make(chan *cmn.DatastoreStored)
+	sub, err := instance.WatchStored(nil, eventChan, []string{})
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Listening for events...")
+	log.Println("-----------------------")
+
+	for {
+		select {
+		case err := <-sub.Err():
+			panic(err)
+		case event := <-eventChan:
+			log.Println("Event occured!!")
+			log.Println("--------------------")
+			log.Printf("No: \033[34m%s\033[0m\n", event.ExamNo)
+			log.Printf("Data: \033[34m%s\033[0m\n", event.Data)
+
+			rw, err := event.Raw.MarshalJSON()
+			if err != nil {
+				panic(err)
+			}
+			log.Printf("Raw log: \033[32m%v\033[0m\n", string(rw))
+			log.Println("--------------------")
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Println("Not enough! 👾")
+		return
+	}
+
+	switch os.Args[1] {
+	case "1":
+		listenWithClient()
+	case "2":
+		listenWithContract()
+	default:
+		log.Println("Not right! 👾")
 	}
 }
