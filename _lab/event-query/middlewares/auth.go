8aa36ddc8601218b10132fc81c1658c158ca956d
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-02-11 22:51:57 +0530

    ⚗️ | use Transactor in auth middleware

--- _lab/event-query/middlewares/auth.go
+++ _lab/event-query/middlewares/auth.go
@@ -1,30 +1,16 @@
 package middlewares
 
 import (
+	"_lab/event-query/common"
 	"context"
-	"crypto/ecdsa"
-	"errors"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func AuthGenerator(client *ethclient.Client, key string) (*bind.TransactOpts, error) {
-	privateKey, err := crypto.HexToECDSA(key[2:])
-	if err != nil {
-		return nil, err
-	}
-
-	publicKey := privateKey.Public()
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		return nil, errors.New("error casting public key to ECDSA")
-	}
-
-	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+func AuthGenerator(client *ethclient.Client, tnr common.Transactor) (*bind.TransactOpts, error) {
+	nonce, err := client.PendingNonceAt(context.Background(), tnr.Address)
 	if err != nil {
 		return nil, err
 	}
@@ -40,7 +26,7 @@ func AuthGenerator(client *ethclient.Client, key string) (*bind.TransactOpts, er
 		return nil, err
 	}
 
-	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+	auth, err := bind.NewKeyedTransactorWithChainID(tnr.Key, chainID)
 	if err != nil {
 		return nil, err
 	}
