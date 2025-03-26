3f2d06992bcd899e7d0b35546d4e49f93b4c76a4
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-03-26 23:33:49 +0530

    ⚗️ | use bind/v2 in AuthGenerator middleware

--- _lab/event-query/middlewares/auth.go
+++ _lab/event-query/middlewares/auth.go
@@ -5,36 +5,34 @@ import (
 	"context"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/v2"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func AuthGenerator(client *ethclient.Client, tnr common.Transactor) (*bind.TransactOpts, error) {
-	nonce, err := client.PendingNonceAt(context.Background(), tnr.Address)
-	if err != nil {
-		return nil, err
-	}
-
-	gasLimit := uint64(9270000)
-	// gasPrice, err := client.SuggestGasPrice(context.Background())
-	// if err != nil {
-	// 	return nil, err
-	// }
-
+func AuthGenerator(client *ethclient.Client, t common.Transactor) (*bind.TransactOpts, error) {
 	chainID, err := client.ChainID(context.Background())
 	if err != nil {
 		return nil, err
 	}
 
-	auth, err := bind.NewKeyedTransactorWithChainID(tnr.Key, chainID)
+	auth := bind.NewKeyedTransactor(t.Key, chainID)
+	nonce, err := client.PendingNonceAt(context.Background(), t.Address)
 	if err != nil {
 		return nil, err
 	}
 
 	auth.Nonce = big.NewInt(int64(nonce))
 	auth.Value = big.NewInt(0)
-	auth.GasLimit = gasLimit
+	auth.GasLimit = uint64(9270000)
 	auth.GasPrice = big.NewInt(0)
 
+	/* In case of no-gas network, comment the following code. */
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	auth.GasPrice = gasPrice
+
 	return auth, nil
+
 }
