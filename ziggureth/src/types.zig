066965ff5db2fefb31a1a8712c51893619bf6112
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-04-27 23:07:43 +0530

    🏷️ | add block header type
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

--- ziggureth/src/types.zig
+++ ziggureth/src/types.zig
@@ -1,3 +1,27 @@
 pub const SimpleResponse = struct {
     result: []u8,
 };
+
+pub const Header = struct {
+    hash: []u8,
+    parentHash: []u8,
+    sha3Uncles: []u8,
+    miner: []u8,
+    stateRoot: []u8,
+    transactionsRoot: []u8,
+    receiptsRoot: []u8,
+    logsBloom: []u8,
+    difficulty: []u8,
+    number: []u8,
+    gasLimit: []u8,
+    gasUsed: []u8,
+    timestamp: []u8,
+    extraData: []u8,
+    mixHash: []u8,
+    nonce: []u8,
+    baseFeePerGas: []u8,
+    withdrawalsRoot: []u8,
+    blobGasUsed: []u8,
+    excessBlobGas: []u8,
+    parentBeaconBlockRoot: []u8,
+};
