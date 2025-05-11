7b1b3851af68f72aa08d2069fdc4e7540f58c698
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-05-11 21:28:30 +0530

    ⚗️ | add cmd/contract for data-pump
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

---
+++ _lab/data-pump/cmd/contract/deploy.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"_lab/data-pump/common"
+	"context"
+	"log"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/v2"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial(common.ProviderURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth, err := common.GenerateAuth(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	deployParams := bind.DeploymentParams{
+		Contracts: []*bind.MetaData{&common.RegistryMetaData},
+	}
+
+	deployer := bind.DefaultDeployer(auth, client)
+	result, err := bind.LinkAndDeploy(&deployParams, deployer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := bind.WaitDeployed(context.Background(), client, result.Txs[common.RegistryMetaData.ID].Hash()); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Contract Address: \033[32m%s\033[0m\n", result.Addresses[common.RegistryMetaData.ID].Hex())
+}
