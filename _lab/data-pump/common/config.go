ec7de2b4f11c28a5b8d569b20a34c2f970378df4
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-05-28 12:21:28 +0530

    ⚗️ | update data-pump common to use values from env
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

--- _lab/data-pump/common/config.go
+++ _lab/data-pump/common/config.go
@@ -5,6 +5,9 @@ import (
 	"crypto/ecdsa"
 	"log"
 	"math/big"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/v2"
 	"github.com/ethereum/go-ethereum/common"
@@ -12,21 +15,9 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-const (
-	ProviderURL       = "http://127.0.0.1:8545"
-	intID       int64 = 31337
-)
-
 var (
-	privateKeys = []string{
-		"ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-		"59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
-		"5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
-		"7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
-		"47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a",
-	}
-
-	ContractAddress = common.HexToAddress("0x5FbDB2315678afecb367f032d93F642f64180aa3")
+	ProviderURL     string
+	ContractAddress common.Address
 	Transactors     []Transactor
 )
 
@@ -57,9 +48,18 @@ func (t *Transactor) NewAuth(client *ethclient.Client) (*bind.TransactOpts, erro
 }
 
 func init() {
-	chainID := big.NewInt(intID)
+	ida := os.Getenv("CHAIN_ID")
+	idi, err := strconv.Atoi(ida)
+	if err != nil {
+		log.Fatalf("Failed to parse chain ID: %v\n", err)
+	}
 
-	for _, k := range privateKeys {
+	chainID := big.NewInt(int64(idi))
+	ProviderURL = os.Getenv("CHAIN_URL")
+	ContractAddress = common.HexToAddress(os.Getenv("CONTRACT_ADDRESS"))
+
+	privateKeys := strings.SplitSeq(os.Getenv("PRIVATE_KEYS"), ",")
+	for k := range privateKeys {
 		privateKey, err := crypto.HexToECDSA(k)
 		if err != nil {
 			log.Fatalf("Failed to parse private key: %v\n", err)
