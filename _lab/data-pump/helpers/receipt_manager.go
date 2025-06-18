1b0afbf935bd25f8ac119a2f7fa734e8d2e72b5e
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-06-18 09:42:15 +0530

    ⚗️ | handle `EOF` error during receipt retrieval
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

--- _lab/data-pump/helpers/receipt_manager.go
+++ _lab/data-pump/helpers/receipt_manager.go
@@ -4,7 +4,9 @@ import (
 	"_lab/data-pump/common"
 	"context"
 	"errors"
+	"io"
 	"log"
+	"net/url"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
@@ -16,12 +18,18 @@ func ReceiptManager(client *ethclient.Client, trx *types.Transaction) error {
 	for {
 		r, err := client.TransactionReceipt(context.Background(), trx.Hash())
 		if err != nil {
-			if errors.Is(err, ethereum.NotFound) {
+			switch err {
+			case ethereum.NotFound:
 				log.Printf("\033[32m[INF]\033[0m Receipt isn't available. Will check after the interval.\n")
 				time.Sleep(common.ReceiptInterval)
 				continue
+			case &url.Error{Err: io.EOF}:
+				log.Printf("\033[32m[WRN]\033[0m Chain isn't responding. Will check after the interval.\n")
+				time.Sleep(common.ReceiptInterval)
+				continue
+			default:
+				return err
 			}
-			return err
 		}
 
 		if r.Status == types.ReceiptStatusSuccessful {
