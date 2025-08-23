df0611d0010fa916d1efc2277ad24a5070fde728
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-08-23 21:52:05 +0530

    ⚗️ | add custom provider type
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

---
+++ _lab/event-query/src/types.rs
@@ -0,0 +1,12 @@
+use alloy::providers::{
+    fillers::{BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller},
+    Identity, RootProvider,
+};
+
+pub type MyProvider = FillProvider<
+    JoinFill<
+        Identity,
+        JoinFill<GasFiller, JoinFill<BlobGasFiller, JoinFill<NonceFiller, ChainIdFiller>>>,
+    >,
+    RootProvider,
+>;
