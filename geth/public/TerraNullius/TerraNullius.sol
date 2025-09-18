18ff4051b9e3a62846f10a646fba924859ddf059
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-09-18 11:31:50 +0530

    🎨 | format TerraNullius contract
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

--- geth/public/TerraNullius/TerraNullius.sol
+++ geth/public/TerraNullius/TerraNullius.sol
@@ -1,14 +1,18 @@
 contract TerraNullius {
-  struct Claim { address claimant; string message; uint block_number; }
-  Claim[] public claims;
+    struct Claim {
+        address claimant;
+        string message;
+        uint block_number;
+    }
+    Claim[] public claims;
 
-  function claim(string message) {
-    uint index = claims.length;
-    claims.length++;
-    claims[index] = Claim(msg.sender, message, block.number);
-  }
+    function claim(string message) {
+        uint index = claims.length;
+        claims.length++;
+        claims[index] = Claim(msg.sender, message, block.number);
+    }
 
-  function number_of_claims() returns(uint result) {
-    return claims.length;
-  }
+    function number_of_claims() returns (uint result) {
+        return claims.length;
+    }
 }
